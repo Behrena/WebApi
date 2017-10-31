be5a9a526978223f747e93193ee566b619e1b368
Behri <Behrij@hotmail.com>
2017-10-31 13:39:11 -0400

    Web Apit Exmaple

---
+++ WebApi/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public string Get()
+        {
+            return "WelCome to web API";
+        }
+
+        public List<string> Get(int Id)
+        {
+            return new List<string>
+            {
+               "data-1",
+               "Data-2"
+            };
+        }
+    }
+}
---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+
+        Product[] products = new Product[]
+        {
+            new Product {Id=1, Name="Tomato Soup" , Category="Groceries", Price=1 },
+            new Product {Id=2, Name="yo-yo" , Category="Toys", Price= 3.13M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 3, Name = "XOXO", Category = "LOve", Price = 10000000000 }
+
+
+        };
+     
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
